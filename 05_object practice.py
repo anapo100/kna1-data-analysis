1758ea8493a5997288a51ef4c689d8fde03d17a0
anapo100 <leehail103@naver.com>
2026-07-15 16:26:00 +0900

    object practice 1~7

---
+++ 05_object practice.py
@@ -0,0 +1,55 @@
+# 실습 1
+count = 3
+temp = 36.5
+name = "홍길동"
+is_ok = True
+
+print(count)
+print(temp)
+print(name)
+print(is_ok)
+
+# 실습 2
+print(type(count)) # <class 'int'>
+print(type(temp))  # <class 'float'>
+print(type(name))  # <class 'str'>
+print(type(is_ok)) # <class 'bool'>
+
+# 실습 3
+100 # type int
+100.0 # type float
+"100" # type str
+
+print(type(100))   # <class 'int'>
+print(type(100.0)) # <class 'float'>
+print(type("100")) # <class 'str'>
+
+# 실습 4
+print(3 + 5) # 8
+print("3" + "5") # 35
+print("32" + "23") # 3223
+
+# 실습 5
+3 > 2 # True
+print(3 > 2) # True
+
+5 = 5 # True
+print(5 == 5) # True
+
+print(type(3 > 2)) # <class 'bool'>
+
+# 실습 6
+num = 123
+print(type(num)) # <class 'int'>
+
+num = 123.0
+print(type(num)) # <class 'float'>
+
+num = "123"
+print(type(num)) # <class 'str'>
+
+# 실습 7
+device_temp = 36.5      # float
+check_count = 3         # int
+device_name = "센서 A"  # str
+is_normal = True        # bool
