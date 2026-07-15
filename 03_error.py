05cce373de6020cde16985ce662ffa84bb1f266d
anapo100 <leehail103@naver.com>
2026-07-15 13:53:42 +0900

    0714 error example

---
+++ 03_error.py
@@ -0,0 +1,28 @@
+# print("==== NameError 만들기 ====")
+
+# # print(NameError 만들기) # NameError: name 'NameError' is not defined
+# print("NameError 만들기") # NameError 만들기 > 글자 출력
+
+# print("==== SyntaxError 만들기 ====")
+
+# print("온도)
+# print("진동"
+
+# print(온도, 75)       #NameError: name '온도' is not defined
+# print("진동",2.3      #SyntaxError: '(' was never closed  
+# print("압력" : 4.0)   #SyntaxError: invalid syntax
+
+# print("온도:", 75)    # 따온표 추가
+# print("진동:", 2.3)   # 괄호 닫기 추가
+# print("압력:", 4.0)   # 콜론 따온표 안으로 
+
+
+# # 점검 리포트
+# # === 1번 설비 점검 === 온도(℃): 82 온도 상승량: 11
+# # 오늘 배운 다양한 출력 방법을 사용해서 위와 같은 간단한 점검 리포트 출력하기
+
+# print("=== 1번 설비 점검 ===")
+# print("온도:", 35.5+46.5)
+# print("온도 상승량:", 46.5-35.5)
+
+
