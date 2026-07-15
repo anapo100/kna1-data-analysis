d6b11c3cb65b4b128b1633be22c7e93b296e0974
anapo100 <leehail103@naver.com>
2026-07-15 15:59:11 +0900

    update object example

--- 05_object.py
+++ 05_object.py
@@ -30,7 +30,6 @@ print(type("센서 A")) # <class 'str'>
 print(type(True))     # <class 'bool'>
 print(type(3 > 2))    # <class 'bool'>
 
-
 num = 123
 fake_num = "123"
 str = "문자열"
@@ -41,3 +40,18 @@ print(fake_num, type(fake_num)) # 123 <class 'str'>
 print(str, type(str))         # 문자열 <class 'str'>
 print(ok, type(ok))           # True <class 'bool'>
 
+# 자료형마다 동작이 다른 것 확인하기
+
+print(3+5) # 8 > 숫자끼리 더하기는 계산
+print("3"+"5") # 35 > 문자열끼리 더하기는 이어붙이기
+print("안녕하"+"세요") # 안녕하세요 
+
+# 자주 하는 실수
+print(0.1 + 0.8) # 0.9 출력되지만 가끔 연산 과정에서 오차가 발생할 수 있음
+
+# 해결법: round() 함수 사용
+print(round(0.1 + 0.8, 2)) # 0.9
+
+# str과 int, float은 덧셈 불가
+# print("123"+456) # TypeError: can only concatenate str (not "int") to str
+print(10 / 2) # 5.0 > 나누기는 항상 실수형으로 나옴
