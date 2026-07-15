7d8b750c637290181595ab959b4cf352ad2c427c
anapo100 <leehail103@naver.com>
2026-07-15 17:51:15 +0900

    operator_practice 1~2

---
+++ 06_operator_practice.py
@@ -0,0 +1,21 @@
+# 실습 1
+a = 10
+b = 3
+
+print(a+b) # 13
+print(a-b) # 7
+print(a*b) # 30
+print(a/b) # 3.3333333333333335
+print(a//b) # 3
+print(a%b) # 1
+print(a**b) # 1000
+
+# 실습 2
+c = 10
+d = 5
+e = 2
+print((c+d+e)/3) # 5.666666666666667
+slide = 8
+print(slide**2) # 64
+
+print(c*d*e) # 100
