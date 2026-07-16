516c194a2c45c5be48ecf08ca6f4878ae7004d44
anapo100 <leehail103@naver.com>
2026-07-16 10:30:57 +0900

    0716 relational, logical operator

--- 06_operator_.py
+++ 06_operator_.py
@@ -47,5 +47,54 @@ print("안녕 "+"하세요") # 안녕 하세요
 # 방법3
 print("안녕" + " " + "하세요") # 안녕 하세요
 
-
 print("안녕"*5) # 안녕안녕안녕안녕안녕
+
+# 비교 연산자
+# <(미만) >(초과) <=(이하) >=(이상) ==(같다) !=(다르다)
+# 비교 연산자는 무조건 True or False (boolean) 결과
+print(7 < 16) # True
+print(7 > 16) # False
+print(7 <= 7) # True
+print(7 >= 7) # True
+print(7 == 7) # True
+print(7 != 7) # False
+
+# 비교 연산자는 문자열 비교도 가능
+print("hello" == "hello") # True
+print("정상" == "정상") # True
+
+# 비교 연산자 사용 문자열 비교시 주의사항
+
+# 1. 대소문자 구분
+print("hello" == "Hello") # False
+
+# 2. 공백도 문자로 취급
+print("정상" == "정상 ") # False
+
+# 부정연산자 != (not)
+print("hello" != "hello") # False
+print("hello" != "hello ") # True
+print("hello" != "Hello") # True
+
+# 변술 비교 연산자 사용 
+num1 = 123
+num2 = 456
+
+print(num1 >= num2) # False
+print(num1 >= "num2") # TypeError: '>=' not supported between instances of 'int' and 'str'
+
+# 논리 연산자
+# and / or / not
+# and : 모두 True 일때만 True
+print(5 == 5 and 7 == 7) # True
+print(5 == 7 and 7 == 7) # False
+print(5 == 5 and 7 != 7) # False
+
+# or : 하나라도 True면 True
+print(5 == 5 or 7 == 7) # True
+print(5 == 7 or 7 == 7) # True
+print(5 == 7 or 7 != 7) # False
+
+# not : True를 False로, False를 True로
+print(not True) # False
+print(not 5 == 5) # False
